b58c114ab98bd7e5003eaf2dfafcfcbcbe00fba4
Landen <landenheine17@gmail.com>
2025-11-19 19:41:56 -0700

    added week 14
    added week 14

---
+++ Week 14/bit_weather_services_6.py
@@ -0,0 +1,156 @@
+"""
+    Name: bit_weather_services_5.py
+    Author: Runtime Rookies
+    Created: 11/12/25
+    Purpose: Week 13 Milestone
+"""
+from tkinter import *
+from tkinter import ttk
+from weather_utils import GetWeather
+
+class BitWeatherServices(Tk):
+    def __init__(self):
+        # initializes
+        super().__init__() # calls __init__ function of the TK (parent) class
+        self.title("Bit Weather Services") # Sets window title
+        self.geometry("873x475") # sets window size
+        self.resizable(False, False) # makes wind not resizable
+
+        # Variables
+        # These will change depending on what the user puts in the entry fields
+        self.state = StringVar() # variable for state
+        self.city = StringVar() # variable for city
+
+        # These are constant
+        BG_COLOR = "lightcyan"
+        ERROR_BG_COLOR = "red"
+
+        # Weather info
+        # These hold the text that will be displayed in the weather results
+        self.conditions_text = StringVar(value="It is ___") # text for weather conditions
+        self.temperature_text = StringVar(value="The temperature is ___") # text for temperature
+        self.wind_speed_text = StringVar(value="The wind is traveling ___") # text for wind speed
+        
+        # Labels
+        # These don't need to be StringVars, but they are for future proofing
+        self.state_text = StringVar(value="Enter State Abbreviation: ") # text for state label
+        self.city_text = StringVar(value="Enter City Name: ") # text for city label
+        self.weather_title_text = StringVar(value="Weather for") # text for title in weather frame
+
+        # Frames
+        self.wf = Frame(self, width=600, height=400, bg=BG_COLOR) # (weather frame) frame to display weather results
+        self.mf = Frame(self, width=600, height=400, bg=BG_COLOR) # (main frame) frame to display main menu
+        self.ef = Frame(self, width=600, height=400, bg=ERROR_BG_COLOR) # (error frame) frame to display error screen
+
+        self.pack_frames()
+
+    def display_weather(self):
+        self.mf.pack_forget() # unpacks main frame
+        self.wf.pack(fill="both", expand=True) # packs weather frame
+
+    def display_menu(self):
+        self.wf.pack_forget() # unpacks weather frame
+        self.mf.pack(fill="both", expand=True) # packs main frame
+
+    def display_error(self):
+        self.mf.pack_forget() # unpacks main frame
+        self.ef.pack(fill="both", expand=True) # packs error frame
+    
+    def error_display_menu(self):
+        self.ef.pack_forget() # unpacks error frame
+        self.mf.pack(fill="both", expand=True) # packs main frame
+
+    def check_inputs(self):
+        # Read Entries and store them as temporary variables
+        city = self.city.get() # stores text in city entry
+        state = self.state.get().strip().upper() # stores text in state entry
+
+        # Check to make sure user did not leave the state or city blank
+        if (city == "" or state == ""):
+            # Send user to error screen if they did
+            self.display_error()
+            return
+        
+        # Use api to get the weather
+        api_handler = GetWeather(city, state)
+        api_handler.get_weather_data()
+
+        # Only runs if api_handler actually gets data
+        # Add an else here to show if user did something wrong
+        if api_handler.weather_data != None:
+            conditions = api_handler.conditions
+            temperature = api_handler.temperature
+            wind_speed = api_handler.wind_speed
+
+            # Title is the only value of all of the results we can update before adding the api
+            self.weather_title_text.set(f"Weather For {city}") # updates value of the title to include the city
+            self.conditions_text.set(f"It is {conditions}") # updates text for weather conditions
+            self.temperature_text.set(f"The temperature is {temperature}°") # updates text for temperature
+            self.wind_speed_text.set(f"The wind is traveling {wind_speed} MPH") # updates text for wind speed
+        
+            self.display_weather() # displays weather results
+        else:
+            self.display_error() # send user to error screen
+
+    def _pack_weather(self):
+        ### WEATHER FRAME
+        # Widgets
+        self.weather_title = ttk.Label(self.wf, textvariable=self.weather_title_text, font=("Brush Script MT", 60), background="gray") # label for title
+        self.conditions_label = ttk.Label(self.wf, textvariable=self.conditions_text, font=("Brush Script MT", 36), background="gray") # label for conditions
+        self.temperature_label = ttk.Label(self.wf, textvariable=self.temperature_text, font=("Brush Script MT", 36), background="gray") # label for temperature
+        self.wind_speed_label = ttk.Label(self.wf, textvariable=self.wind_speed_text, font=("Brush Script MT", 36), background="gray") # label for wind speed
+        self.wind = ttk.Label(self.wf, image=self.wind_img, background="gray") # label with wind image
+        self.return_to_menu_button = ttk.Button(self.wf, text="Back", width=12, command=self.display_menu) # Button to return to the main menu
+        # Packing
+        self.return_to_menu_button.grid(row=0, column=0, padx=10, pady=10, sticky="nw") # packs button to return
+        self.weather_title.grid(row=1, column=1, padx=20, pady=20) # packs title label
+        self.conditions_label.grid(row=2, column=1, padx=10, pady=10) # packs conditions label
+        self.temperature_label.grid(row=3, column=1, padx=10, pady=10) # packs temperature label
+        self.wind_speed_label.grid(row=4, column=1, padx=10, pady=10) # packs wind speed label
+        self.wind.grid(row=4, column=0, sticky="e", padx=10, pady=10) # packs wind image
+
+    def _pack_menu(self):
+        ### MENU FRAME
+        # images
+        self.title_img = PhotoImage(file="images/title.png") # grabs image for the title in menu
+        self.cloud_img = PhotoImage(file="images/weather_button.png") # grabs image for the button to get weather
+        self.wind_img = PhotoImage(file="images/wind.png") # grabs image for the wind
+
+        # Widgets
+        self.menu_title = ttk.Label(self.mf, image=self.title_img, width=600, background="gray") # label showing title
+        self.city_label = ttk.Label(self.mf, textvariable=self.city_text, font=("Brush Script MT", 24), background="gray") # label for city
+        self.state_label = ttk.Label(self.mf, textvariable=self.state_text, font=("Brush Script MT", 24), background="gray") # label for state
+        self.city_input = ttk.Entry(self.mf, textvariable=self.city, font=("Helvetica", 36)) # entry for city
+        self.state_input = ttk.Entry(self.mf, textvariable=self.state, font=("Helvetica", 36)) # entry for state
+        self.get_weather_button = ttk.Button(self.mf, command=self.check_inputs, image=self.cloud_img) # button with cloud image to get weather
+        # Packing
+        self.menu_title.grid(row=0, column=0, columnspan=2, sticky="ew", padx=8, pady=5) # packs title
+        self.city_label.grid(row=1, column=0, sticky="s", padx=20, pady=(20, 0)) # packs city label
+        self.city_input.grid(row=2, column=0, sticky="ew", padx=20) # packs city entry
+        self.state_label.grid(row=1, column=1, sticky="s", padx=20, pady=(20, 0)) # packs state label
+        self.state_input.grid(row=2, column=1, sticky="ew", padx=20) # packs state entry
+        self.get_weather_button.grid(row=3, column=0, columnspan=2, pady=30, ipadx=5, ipady=5) # packs button to check weather
+        # Configuring
+        self.mf.columnconfigure(0, weight=1) # ensures both columns expand equally
+        self.mf.columnconfigure(1, weight=1) # they keep the text for the input labels nice
+
+    def _pack_error(self):
+        ### ERROR FRAME
+        # Widgets
+        self.error_text = ttk.Label(self.ef, text="ERROR! PLEASE ENTER A\nVALID CITY AND/OR STATE!", font=("Impact", 60), background="red") # label for error message
+        self.error_to_menu_button = ttk.Button(self.ef, text="Back", width=12, command=self.error_display_menu) # Button to return to the main menu
+        # Packing
+        self.error_text.grid(row=0, column=0, padx=10, pady=10) # packs error message label
+        self.error_to_menu_button.grid(row=1, column=0, padx=10, pady=10) # packs button to return to menu
+
+    def pack_frames(self):
+        self._pack_menu()
+        self._pack_error()
+        self._pack_weather()
+
+
+
+if __name__ == "__main__":
+    weather = BitWeatherServices() # creates instance of class
+    weather.display_menu() # display menu frame
+    weather.mainloop() # starts tkinters loop so it works
---
+++ Week 14/weather_utils.py
@@ -0,0 +1,105 @@
+"""
+    Name: weather_utils.py
+    Author: Noel Onate
+    Created: 4/2/25
+    Purpose: Store OpenWeatherMap API key and URL for
+    easy import into other OpenWeatherMap programs
+"""
+
+# OpenWeatherMap API Key
+API_KEY = "cbbcc7613091e76023dc824acdd2419c"
+
+# URL to access current weather OpenWeatherMap API
+WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
+GEOMAP_URL = "http://api.openweathermap.org/geo/1.0/direct"
+
+import requests, json
+
+class GetWeather():
+    def __init__(self, city, state):
+        self.city = city
+        self.state = state
+        self.latitude = None
+        self.longitude = None
+        self.weather_data = None
+    
+    def set_location(self, city, state):
+        self.city = city
+        self.state = state
+    
+    def get_coordinates(self):
+        try:
+            query_string = {
+                "q": self.get_location(),        # Location for weather
+                "limit": 5,
+                "appid": "295ba02a2d7c1e90d464ea247bfad517"
+            }
+
+            response = requests.get(
+                GEOMAP_URL,
+                params=query_string
+            )
+
+            if response.status_code == 200:
+                self.location_data = response.json()[0]
+                self.latitude = self.location_data["lat"]
+                self.longitude = self.location_data["lon"]
+                print(f"[+] Coordinates found: {self.latitude}, {self.longitude}")
+            else:
+                print(f" Response code: {response.status_code}")
+                print(" You may have typed an invalid location.")
+                print(" Please try again.")
+
+            # Update latitude and longitude
+        
+        except:
+            print("[-] Sorry, there was a problem connecting")
+
+    def get_location(self):
+        return f"{self.city}, {self.state}, US"
+    
+    def get_weather_data(self):
+        try:
+
+            # Build the openweathermap request parameters
+            # These are added on to the URL to make the complete request
+            query_string = {
+                "units": "imperial",        # Units of measure ex: Fahrenheit
+                "q": self.get_location(),         # Location for weather
+                "appid": API_KEY
+            }
+
+            # Get the API JSON data as a Python JSON object
+            response = requests.get(
+                WEATHER_URL,
+                params=query_string
+            )
+            
+            # If the status_code is 200, successful connection and data
+            if response.status_code == 200:
+
+                # Get json response into a Python dictionary
+                self.weather_data = response.json()
+
+                # Let user know the connection was successful
+                print("\n [+] Connection successful.")
+            else:
+                print(f" Response code: {response.status_code}")
+                print(" You may have typed an invalid location.")
+                print(" Please try again.")
+
+
+            # Get weather items from dictionaries
+            self.temperature = self.weather_data.get("main").get("temp")
+            self.conditions = self.weather_data.get("weather")[0].get("main")
+            self.wind_speed = self.weather_data.get("wind").get("speed")
+
+        except:
+            # Handle any exceptions
+            print("[-] Sorry, there was a problem connecting.")
+            self.weather_data = None
+
+if __name__ == "__main__":
+    test = GetWeather("NE", "Scottsbluff")
+    test.get_coordinates()
+    print(test.latitude, test.longitude)
